274990066edff673196b4875faf98afb703869fa
Darius Carter <dcarterbusinessemail@gmail.com>
2018-08-15 13:18:20 -0500

    Undone

---
+++ js.js
@@ -0,0 +1,12 @@
+
+
+var doID = document.getElementById("first").innerHTML;
+console.log(doID);
+
+
+var doClass = document.getElementsByClassName("special");
+document.getElementsByClassName("special").innerHTML;
+
+
+var doTag = document.getElementsByTagName("p").innerHTML;
+console.log(doTag);
